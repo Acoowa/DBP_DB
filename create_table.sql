563701bf8ebcff073cfc632868e8bdc91ec7eb1e
evgenii <evgenii.zholobov@gmail.com>
2024-04-17 10:13:32 +0500

    refactoring

--- create_table.sql
+++ create_table.sql
@@ -1,41 +1,38 @@
 -- Create regions table
-CREATE TABLE Regions (
-    id serial PRIMARY KEY,
+CREATE TABLE Regions
+(
+    id   serial PRIMARY KEY,
     name VARCHAR
 );
 
 -- Create locations tale
-CREATE TABLE Locations (
-    id serial PRIMARY KEY,
-    name VARCHAR,
-    adress VARCHAR,
-    region_id INTEGER,
-
-    FOREIGN KEY (region_id) REFERENCES Regions (id)
+CREATE TABLE Locations
+(
+    id        serial PRIMARY KEY,
+    name      VARCHAR,
+    address   VARCHAR,
+    region_id INTEGER REFERENCES Regions (id)
 );
 
 -- Create Departments table
 
-CREATE TABLE Departments (
-    id serial PRIMARY KEY,
-    name VARCHAR,
-    location_id INTEGER,
-    manager_id INTEGER,
-
-    FOREIGN KEY (location_id) REFERENCES Locations (id)
+CREATE TABLE Departments
+(
+    id          serial PRIMARY KEY,
+    name        VARCHAR,
+    location_id INTEGER REFERENCES Locations (id),
+    manager_id  INTEGER
 );
 
 --Create employees table
-CREATE TABLE Employees (
-    id serial PRIMARY KEY,
-    name VARCHAR,
-    last_name VARCHAR,
-    hire_date DATE,
-    salary INTEGER,
-    email VARCHAR,
-    manager_id INTEGER,
-    department_id INTEGER,
-
-    FOREIGN KEY (manager_id) REFERENCES Employees (id),
-    FOREIGN KEY (department_id) REFERENCES Departments (id)
+CREATE TABLE Employees
+(
+    id            serial PRIMARY KEY,
+    name          VARCHAR,
+    last_name     VARCHAR,
+    hire_date     DATE,
+    salary        INTEGER,
+    email         VARCHAR,
+    manager_id    INTEGER REFERENCES Employees (id),
+    department_id INTEGER REFERENCES Departments (id)
 );
--- queries.sql
+++ queries.sql
@@ -1,34 +1,33 @@
 -- Показать работников у которых нет почты или почта не в корпоративном домене (домен dualbootpartners.com)
-SELECT * FROM Employees 
-WHERE email IS NULL or NOT (email LIKE '%dualbootpartners.com');
+SELECT *
+FROM Employees
+WHERE email IS NULL
+   OR NOT (email LIKE '%dualbootpartners.com');
 
 -- Получить список работников нанятых в последние 30 дней
-SELECT * FROM Employees
-WHERE hire_date >= current_date - interval '30 days';
+SELECT *
+FROM Employees
+WHERE hire_date >= CURRENT_DATE - INTERVAL '30 days';
 
 -- Найти максимальную и минимальную зарплату по каждому департаменту
-SELECT
-    Departments.name as department_name, 
-    MAX(salary) AS max_salary, 
-    MIN(salary) AS min_salary
+SELECT Departments.name AS department_name,
+       MAX(salary)      AS max_salary,
+       MIN(salary)      AS min_salary
 FROM Employees
-JOIN Departments
-ON Employees.department_id = Departments.id
+         JOIN Departments
+              ON Employees.department_id = Departments.id
 GROUP BY department_name;
 
 -- Посчитать количество работников в каждом регионе
-SELECT 
-    Regions.name as region_name,
-    COUNT(employees.id) as employee_count
-FROM
-    Employees,
-    Departments,
-    Locations,
-    Regions
-WHERE
-    Regions.id = Locations.region_id
-    AND Locations.id = Departments.location_id
-    AND Departments.id = Employees.department_id
+SELECT Regions.name        AS region_name,
+       COUNT(employees.id) AS employee_count
+FROM Employees,
+     Departments,
+     Locations,
+     Regions
+WHERE Regions.id = Locations.region_id
+  AND Locations.id = Departments.location_id
+  AND Departments.id = Employees.department_id
 GROUP BY Regions.name;
 
 -- Показать сотрудников у которых фамилия длиннее 5 символов
